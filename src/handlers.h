247ccc212ea0e32eb9831de0ac0392fc1edeff4d
tangjinjing <tangjinjing@deepexi.com>
2020-11-08 22:21:27 +0800

    所有请求处理放入 general_request_handler

--- src/handlers.h
+++ src/handlers.h
@@ -6,7 +6,6 @@
 
 void general_request_handler(struct evhttp_request *req, void *arg);
 void ping_handler(struct evhttp_request *req, void *args);
-void post_handler(struct evhttp_request *req, void *args);
 
 typedef void (*_request_handler_)(struct evhttp_request *req, void *arg);
 struct request_handler_t {
@@ -17,7 +16,6 @@ struct request_handler_t {
 
 static request_handler_t request_handlers [] = {
     { "/ping", ping_handler, NULL },
-    { "/post", post_handler, NULL },
     // ... more uri handlers ...
     { NULL, NULL, NULL},
 };
